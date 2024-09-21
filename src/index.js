69fca316f07fe1e281f66e6cdba80fa24762cb03
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-09-21 20:24:53 +0800

    Adjusting the navbar layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import App from "./App";
 
-    <App />
-
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
