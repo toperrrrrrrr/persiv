413f19a717eabb7464df78c1182fa6118b195edc
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-09-23 20:19:15 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logos from './assets/imgs/logo.png';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
    return (
@@ -8,19 +9,18 @@ function App() {
             <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
                <a href="index.html" class="logo d-flex align-items-center me-auto">
-
                   <img src={logos} alt="" />
-                  <h1 class="sitename">Persiv</h1>
+                  <h1 class="sitename">FlexStart</h1>
                </a>
 
                <nav id="navmenu" class="navmenu">
                   <ul>
-                     <li><a href="#hero" class="active">Home</a></li>
+                     <li><a href="#hero" class="active">Home<br /></a></li>
                      <li><a href="#about">About</a></li>
-                     <li><a href="#services">SERVICES</a></li>
+                     <li><a href="#services">Services</a></li>
                      <li><a href="#portfolio">Portfolio</a></li>
                      <li><a href="#team">Team</a></li>
-                     <li><a href="#pricing">Pricing</a></li>
+                     <li><a href="blog.html">Blog</a></li>
                      <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
                         <ul>
                            <li><a href="#">Dropdown 1</a></li>
@@ -38,12 +38,41 @@ function App() {
                            <li><a href="#">Dropdown 4</a></li>
                         </ul>
                      </li>
+                     <li class="listing-dropdown"><a href="#"><span>Listing Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                           <li>
+                              <a href="#">Column 1 link 1</a>
+                              <a href="#">Column 1 link 2</a>
+                              <a href="#">Column 1 link 3</a>
+                           </li>
+                           <li>
+                              <a href="#">Column 2 link 1</a>
+                              <a href="#">Column 2 link 2</a>
+                              <a href="#">Column 3 link 3</a>
+                           </li>
+                           <li>
+                              <a href="#">Column 3 link 1</a>
+                              <a href="#">Column 3 link 2</a>
+                              <a href="#">Column 3 link 3</a>
+                           </li>
+                           <li>
+                              <a href="#">Column 4 link 1</a>
+                              <a href="#">Column 4 link 2</a>
+                              <a href="#">Column 4 link 3</a>
+                           </li>
+                           <li>
+                              <a href="#">Column 5 link 1</a>
+                              <a href="#">Column 5 link 2</a>
+                              <a href="#">Column 5 link 3</a>
+                           </li>
+                        </ul>
+                     </li>
                      <li><a href="#contact">Contact</a></li>
                   </ul>
                   <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
                </nav>
 
-               <a class="btn-getstarted" href="#about">Get Started</a>
+               <a class="btn-getstarted flex-md-shrink-0" href="index.html#about">Get Started</a>
 
             </div>
          </header>
@@ -53,3 +82,4 @@ function App() {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./styles.css";
 
 import App from "./App";
